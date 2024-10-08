e3dbfa79e7f6d61dad06649a90da3087faef5d2d
ShreyaInnofied2024 <shreya.singh@innofied.com>
2024-10-08 11:30:07 +0530

    Delete Delete directory

--- Delete/Delete.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <body>
-    <form action="/OOPS/Object.php" method="post">
-    <p> Enter the ProductId you want to delete</p>
-      ProductId: <input type="text" name="Id"<br>
-      <input type="submit">
-    </form>
-  </body>
-</html>
