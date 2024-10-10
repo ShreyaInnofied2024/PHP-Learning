18b3a97d69e9f88cc26b462b293eca95fae64c74
ShreyaInnofied2024 <shreya.singh@innofied.com>
2024-10-10 17:20:35 +0530

    Update Static.php

--- OOPS/Static.php
+++ OOPS/Static.php
@@ -4,8 +4,8 @@ class Area_Circle
     public static $pi = 3.14159;
     public static function CalculateArea($radius)
     {
-        return self::$pi * $radius * $radius;
+        return self::$pi * $radius * $radius;      //static variable called within class
     }
 }
 
-echo Area_Circle::CalculateArea(10);
+echo Area_Circle::CalculateArea(10);              //static function called outside class
