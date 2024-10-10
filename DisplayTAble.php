3e3cda048e09c7867d5b3d86be084d7452afe403
ShreyaInnofied2024 <shreya.singh@innofied.com>
2024-10-10 17:09:41 +0530

    Update DisplayTAble.php

--- DisplayTAble.php
+++ DisplayTAble.php
@@ -5,14 +5,14 @@ $password = "password";
 $db_name = "Products";
 
 try {
-    $conn = new PDO("mysql:host=$server_name;dbname=$db_name", $user_name, $password);
+    $conn = new PDO("mysql:host=$server_name;dbname=$db_name", $user_name, $password);    //Database Connection
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "SELECT ProductId, ProductName, Quantity, Price FROM Product";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);    // if result is present
     if (count($result) > 0) {
-        echo "<table>";
+        echo "<table>";                    //Present the output in the form of a table
         echo "<tr><th>ProductId</th><th>ProductName</th><th>Quantity</th><th>Price</th></tr>";
         foreach ($result as $row) {
             echo "<tr>";
@@ -24,7 +24,7 @@ try {
         }
         echo "</table>";
     } else {
-        echo "No products found in the database.";
+        echo "No products found in the database.";       // if result not found
     }
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
